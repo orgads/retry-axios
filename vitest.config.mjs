a9f184c7ef68c49f6cb8c0a8b8d3ed4b72009049
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2025-06-08 20:53:32 +0300

    test: Migrate tests to vitest

---
+++ vitest.config.mjs
@@ -0,0 +1,21 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+
+/** @type {import('vite').UserConfig} */
+export default defineConfig({
+  test: {
+    include: ['test/**/*.ts', 'test/**/*.cjs'],
+    testTimeout: 60_000,
+    globals: true,
+    coverage: {
+      include: ['src/**/*.ts'],
+      reporter: ['text-summary', 'lcov'],
+      reportsDirectory: 'coverage'
+    }
+  },
+  plugins: [
+    swc.vite({
+      module: { type: 'nodenext' }
+    })
+  ]
+});
